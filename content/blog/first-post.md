fd6f41feb5f8e77b2e7dee297262e0c442a40f7e
Bertyn Boulikou <bboulikou@gmail.com>
2022-09-07 22:43:10 +0200

    fix path render index with nuxt/content

---
+++ content/blog/first-post.md
@@ -0,0 +1,2 @@
+# My first blog post
+Welcome to my first blog post using content v2 module
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
+  modules: ["@nuxt/content-edge", "@nuxtjs/tailwindcss"],
   content: {},
   tailwindcss: {
     cssPath: "~/assets/css/main.css",
