bd1e888b000cfdac85062a9110d948a8d0974092
bertyn99 <bboulikou@gmail.com>
2022-09-18 13:06:07 +0200

    ajout systeme layout

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtPage />
+  <div class="p-2 md:mx-24">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: { default: "#D9D9D9", darken: "#A6A6A6" },
+        secondary: "#262626",
+        tertiary: {
+          default: "#F23005",
+          darken: "#D93D1A",
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
