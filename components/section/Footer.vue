eafc87a6b9b0170ca291c2d06d1f8a36c13bd229
bertyn99 <bboulikou@gmail.com>
2022-10-02 11:07:59 +0200

    test link

--- components/section/Footer.vue
+++ components/section/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-secondary">
+  <footer class="bg-black">
     <div
       class="container mx-auto py-4 flex items-center justify-between text-primary-darken"
     >
@@ -7,7 +7,9 @@
         <p>@All rigth reserved By <strong> ScireDev</strong></p>
       </div>
       <ul class="flex">
-        <li class="mx-2 capitalize"><NuxtLink>Road to basic</NuxtLink></li>
+        <li class="mx-2 capitalize">
+          <NuxtLink to="/roadtobasic">Road to basic</NuxtLink>
+        </li>
         <li class="mx-2 capitalize"><NuxtLink>One to One</NuxtLink></li>
         <li class="mx-2 capitalize">
           <NuxtLink>Tips and advice</NuxtLink>
