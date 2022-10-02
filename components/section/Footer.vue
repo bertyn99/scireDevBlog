c9b9201a063c0577ac94a18afb9bb9a4902fec01
Bertyn Boulikou <bboulikou@gmail.com>
2022-10-02 14:12:52 +0200

    fix responsive footer

--- components/section/Footer.vue
+++ components/section/Footer.vue
@@ -1,12 +1,12 @@
 <template>
   <footer class="bg-black  w-full mt-auto">
     <div
-      class="container mx-auto py-4 flex items-center justify-between text-primary-darken"
+      class="container mx-auto py-4 flex flex-col-reverse sm:flex-row items-center justify-between text-primary-darken"
     >
       <div>
         <p>@All rigth reserved By <strong> ScireDev</strong></p>
       </div>
-      <ul class="flex">
+      <ul class="sm:flex">
         <li class="mx-2 capitalize">
           <NuxtLink to="/roadtobasic">Road to basic</NuxtLink>
         </li>
