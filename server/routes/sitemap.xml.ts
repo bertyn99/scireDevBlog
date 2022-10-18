a05f8b9ccbd1596f9eee705fd13751a97ff882a8
Bertyn Boulikou <bboulikou@gmail.com>
2022-10-18 11:38:54 +0200

    fix name  file and fix sitemap

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -6,10 +6,24 @@ export default defineEventHandler(async (event) => {
   const sitemap = new SitemapStream({
     hostname: "https://sciredev.com",
   });
+
+  sitemap.write({
+    url: "/",
+    changefreq: "weekly",
+    priority: 1,
+  });
   for (const doc of docs) {
     sitemap.write({
       url: doc._path,
       changefreq: "weekly",
+      priority: 0.7,
+      img: [
+        {
+          url: doc.img,
+          caption: "An image",
+          title: doc.title,
+        },
+      ],
     });
   }
   sitemap.end();
