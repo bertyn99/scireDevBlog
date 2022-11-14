c57216439e92950228fe7237eb9032910cd936de
bertyn99 <bboulikou@gmail.com>
2022-11-14 23:12:06 +0100

    fix popular article

--- components/Hero.vue
+++ components/Hero.vue
@@ -22,102 +22,87 @@
       </span>
     </div>
     <div class="md:col-span-2 md:row-span-3">
-      <div class="bg-secondary h-full pb-10 pt-8">
-        <div class="w-full border-l-4 border-tertiary-default">
+      <div class="bg-secondary h-full pb-14 pt-8">
+        <div class="w-full border-l-4 border-tertiary-default py-2">
           <span class="pl-6 font-semibold text-primary-default"
             >Popular Articles</span
           >
         </div>
-        <div v-for="article in popular" class="flex my-1 p-4 h-1/3">
-          <div
-            class="relative w-1/2 md:w-1/3 min-h-[80px] min-w-[90px] aspect-video"
+        <div class="">
+          <NuxtLink
+            v-for="article in popular"
+            class="flex items-center p-3 h-1/3 hover:bg-primary-default/25"
+            :to="article._path"
           >
-            <img
-              :src="article.image"
-              alt=""
-              width="
+            <div
+              class="relative w-1/2 md:w-1/3 min-h-[80px] min-w-[90px] aspect-video"
+            >
+              <img
+                :src="article.image"
+                alt=""
+                width="
           380"
-              height="200"
-              class="absolute top-2 h-4/5 w-full z-20"
-            />
-          </div>
-          <div
-            class="w-1/2 md:w-2/3 h-full flex flex-col p-2 items-center justify-around hover:bg-primary-default/25 z-0 -translate-x-2 lg:-translate-x-8"
-          >
-            <div>
-              <span class="text-primary-default">{{ article.title }}</span>
+                height="200"
+                class="absolute top-2 h-4/5 w-full z-20"
+              />
             </div>
-            <div class="flex pb-1 gap-4">
-              <div class="flex text-primary-darken items-center gap-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-4 h-4"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-
-                <span class="text-sm"> 200 </span>
+            <div
+              class="w-1/2 md:w-2/3 h-full flex flex-col p-2 items-center gap-4 z-0 -translate-x-2 lg:-translate-x-6"
+            >
+              <div class="w-full text-center leading-4">
+                <span class="text-primary-default">{{ article.title }}</span>
               </div>
-              <div class="flex text-primary-darken items-center gap-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-4 h-4"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                  />
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                </svg>
+              <div class="flex pb-1 gap-4">
+                <div class="flex text-primary-darken items-center gap-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-4 h-4"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
 
-                <span class="text-primary-darken text-sm"> 200 </span>
+                  <span class="text-sm"> 200 </span>
+                </div>
+                <div class="flex text-primary-darken items-center gap-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-4 h-4"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                    />
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+
+                  <span class="text-primary-darken text-sm"> 200 </span>
+                </div>
               </div>
             </div>
-          </div>
+          </NuxtLink>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script setup>
-const popularArticle = ref([
-  {
-    title: " Top extension chrome",
-    img: "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Ym9va3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-    time: "4min",
-    view: "200",
-  },
-  {
-    title: " Top extension vscode",
-    img: "https://images.unsplash.com/photo-1481627834876-b7833e8f5570?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Ym9va3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-    time: "4min",
-    view: "200",
-  },
-  {
-    title: " Top extension vscode",
-    img: "https://images.unsplash.com/photo-1607706189992-eae578626c86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dnNjb2RlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    time: "4min",
-    view: "200",
-  },
-]);
-
 const { data } = await useAsyncData("home", () =>
   queryContent({ path: "/blog" }).limit(5).find()
 );
