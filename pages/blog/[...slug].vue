a82a2d8d2550ef096a30b647b31c48187939afd7
Bertyn Boulikou <bboulikou@gmail.com>
2023-01-15 15:31:28 +0100

    fix

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -62,7 +62,8 @@ useHead({
     {
       property: "og:title",
       content: "ScireDev - " + data.value.article.title,
-    }, {
+    },
+    {
       property: "og:description",
       content: data.value.article.description,
     },
@@ -76,7 +77,6 @@ useHead({
       content: data.value.article.author,
     },
     {
-
       property: "og:image",
       content: `https://sciredev.com/${data.value.article.image}`,
     },
@@ -96,7 +96,7 @@ useHead({
     },
     {
       property: "twitter:description",
-      content: data.value.article.description
+      content: data.value.article.description,
     },
     {
       property: "twitter:image",
@@ -105,63 +105,68 @@ useHead({
     },
   ],
 });
-
-/* useJsonld({
-  "@context": "https://schema.org",
-  "@type": "Article",
-  name: "ScireDev",
-  description:
-    "Welcome to scireDev the website that share with you the key to become a better developper. Come learn with us",
-  author: {
-    "@type": "Person",
-    name: data.value.article.author,
-  },
-}); */
 </script>
 <template>
-  <SchemaOrgBreadcrumb :itemListElement="[
-  { name: 'Home', item: '/' },
-  { name: 'Blog', item: '/blog' },
-  { name: data.article.title, item: path },
-]" />
-  <SchemaOrgArticle type="TechArticle" :datePublished="data.article.createdAt" :dateModified="data.article.modifiedAt"
+  <SchemaOrgBreadcrumb
+    :itemListElement="[
+      { name: 'Home', item: '/' },
+      { name: 'Blog', item: '/blog' },
+      { name: data.article.title, item: path },
+    ]"
+  />
+  <SchemaOrgArticle
+    type="TechArticle"
+    :datePublished="data.article.createdAt"
+    :dateModified="data.article.modifiedAt"
     :author="{
-  name: data.article.author,
-  image: getAuthorImg(data.article.author),
-}" />
+      name: data.article.author,
+      image: getAuthorImg(data.article.author),
+    }"
+  />
 
   <main id="main" class="p-4 max-w-5xl mx-auto mt-10">
     <header v-if="data.article.title" class="p-4 pb-12">
       <div class="h-[420px] w-full">
-        <img :src="`/${data.article.image}`" :alt="data.article.title"
-          class="w-full h-full object-fill aspect- rounded-2xl" />
+        <img
+          :src="`/${data.article.image}`"
+          :alt="data.article.title"
+          class="w-full h-full object-fill aspect- rounded-2xl"
+        />
       </div>
       <h1 class="font-extrabold text-5xl my-3">
         {{ capitalize(data.article.title) }}
       </h1>
 
       <ul class="flex gap-4">
-        <li class="bg-slate-500 px-2 py-0.5 rounded-md font-medium text-gray-300" v-for="(tag, n) in data.article.tags"
-          :key="n">
+        <li
+          class="bg-slate-500 px-2 py-0.5 rounded-md font-medium text-gray-300"
+          v-for="(tag, n) in data.article.tags"
+          :key="n"
+        >
           {{ tag }}
         </li>
       </ul>
       <p class="mt-2 text-primary-darken">
         Modified at
         <time itemprop="dateModified" datetime="data.article.modifiedAt">{{
-    data.article.modifiedAt
-}}</time>
+          data.article.modifiedAt
+        }}</time>
       </p>
     </header>
 
     <hr />
 
     <section class="grid grid-cols-8">
-      <aside class="col-span-full md:col-span-2 row-start-1 w-full pt-14" v-if="data.article.body?.toc?.links">
+      <aside
+        class="col-span-full md:col-span-2 row-start-1 w-full pt-14"
+        v-if="data.article.body?.toc?.links"
+      >
         <!-- Toc Component -->
         <Toc :links="data.article.body.toc.links" />
       </aside>
-      <article class="col-span-full md:col-span-6 md:col-start-1 md:row-start-1 prose w-full p-4 max-w-3xl m-auto">
+      <article
+        class="col-span-full md:col-span-6 md:col-start-1 md:row-start-1 prose w-full p-4 max-w-3xl m-auto"
+      >
         <ContentDoc>
           <template #not-found>
             <h1>Document not found</h1>
@@ -178,6 +183,4 @@ useHead({
     <NavPrevNext :prev="prev" :next="next" />
   </main>
 </template>
-<style scoped>
-
-</style>
+<style scoped></style>
