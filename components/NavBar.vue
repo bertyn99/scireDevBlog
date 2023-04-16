6bc29c8ad3b92b9afb1aa5efc6a12f50f36aac47
bertyn99 <bboulikou@gmail.com>
2023-04-16 22:44:45 +0200

    fix: change a to nuxt link

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -4,10 +4,10 @@
     aria-label="Global"
   >
     <div class="flex lg:flex-1">
-      <a href="#" class="-m-1.5 p-1.5">
+      <NuxtLink to="/" class="-m-1.5 p-1.5">
         <span class="sr-only">Scire dev</span>
         <img class="h-14 w-auto" src="/img/scire_logo_primary.png" alt="" />
-      </a>
+      </NuxtLink>
     </div>
     <div class="flex lg:hidden">
       <button
@@ -24,12 +24,12 @@
       </button>
     </div>
     <div class="hidden lg:flex lg:gap-x-12">
-      <a
+      <NuxtLink
         v-for="item in navigation"
         :key="item.name"
         :href="item.href"
         class="text-sm font-semibold leading-6 text-gray-900"
-        >{{ item.name }}</a
+        >{{ item.name }}</NuxtLink
       >
     </div>
     <div class="hidden lg:flex lg:flex-1 lg:justify-end">
@@ -44,14 +44,14 @@
       class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
     >
       <div class="flex items-center justify-between">
-        <a href="#" class="-m-1.5 p-1.5">
+        <NuxtLink href="#" class="-m-1.5 p-1.5">
           <span class="sr-only">Your Company</span>
           <img
             class="h-8 w-auto"
             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
             alt=""
           />
-        </a>
+        </NuxtLink>
         <button
           type="button"
           class="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -64,19 +64,19 @@
       <div class="mt-6 flow-root">
         <div class="-my-6 divide-y divide-gray-500/10">
           <div class="space-y-2 py-6">
-            <a
+            <NuxtLink
               v-for="item in navigation"
               :key="item.name"
               :href="item.href"
               class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-              >{{ item.name }}</a
+              >{{ item.name }}</NuxtLink
             >
           </div>
           <div class="py-6">
-            <a
+            <NuxtLink
               href="#"
               class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-              >Log in</a
+              >Log in</NuxtLink
             >
           </div>
         </div>
