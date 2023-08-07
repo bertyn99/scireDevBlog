f1fe266074051ed5ecf4472045ba4d4c9ec23cf4
Bertyn Boulikou <bboulikou@gmail.com>
2023-08-07 22:40:46 +0200

    fix icon

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -45,12 +45,8 @@
     >
       <div class="flex items-center justify-between">
         <NuxtLink href="#" class="-m-1.5 p-1.5">
-          <span class="sr-only">Your Company</span>
-          <img
-            class="h-8 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt=""
-          />
+          <span class="sr-only">Sciredev</span>
+          <img class="h-10 w-auto" src="/img/scire_logo_primary.png" alt="" />
         </NuxtLink>
         <button
           type="button"
