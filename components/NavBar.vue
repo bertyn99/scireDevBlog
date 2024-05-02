dab7c2fd4b9864dd387b067d495ab28e4f9f369a
bertyn99 <bboulikou@gmail.com>
2024-05-02 17:11:51 +0200

    fix link navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,8 +1,5 @@
 <template>
-  <nav
-    class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
-    aria-label="Global"
-  >
+  <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
     <div class="flex lg:flex-1">
       <NuxtLink to="/" class="-m-1.5 p-1.5">
         <span class="sr-only">Scire dev</span>
@@ -10,27 +7,15 @@
       </NuxtLink>
     </div>
     <div class="flex lg:hidden">
-      <button
-        type="button"
-        class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-        @click="show = true"
-      >
+      <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+        @click="show = true">
         <span class="sr-only">Open main menu</span>
-        <Icon
-          name="heroicons:bars-3-20-solid"
-          class="h-6 w-6"
-          aria-hidden="true"
-        />
+        <Icon name="heroicons:bars-3-20-solid" class="h-6 w-6" aria-hidden="true" />
       </button>
     </div>
     <div class="hidden lg:flex lg:gap-x-12">
-      <NuxtLink
-        v-for="item in navigation"
-        :key="item.name"
-        :href="item.href"
-        class="text-sm font-semibold leading-6 text-gray-900"
-        >{{ item.name }}</NuxtLink
-      >
+      <NuxtLink v-for="item in navigation" :key="item.name" :to="item.href"
+        class="text-sm font-semibold leading-6 text-gray-900">{{ item.name }}</NuxtLink>
     </div>
     <div class="hidden lg:flex lg:flex-1 lg:justify-end gap-x-4">
       <UButton to="/auth/login" variant="outline">Log in</UButton>
@@ -40,18 +25,13 @@
   <div class="lg:hidden" v-if="show">
     <div class="fixed inset-0 z-50" />
     <div
-      class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
-    >
+      class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
       <div class="flex items-center justify-between">
-        <NuxtLink href="#" class="-m-1.5 p-1.5">
+        <NuxtLink to="#" class="-m-1.5 p-1.5">
           <span class="sr-only">Sciredev</span>
           <img class="h-10 w-auto" src="/img/scire_logo_primary.png" alt="" />
         </NuxtLink>
-        <button
-          type="button"
-          class="-m-2.5 rounded-md p-2.5 text-gray-700"
-          @click="show = false"
-        >
+        <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="show = false">
           <span class="sr-only">Close menu</span>
           <Icon name="heroicons:bars-3" class="h-6 w-6" aria-hidden="true" />
         </button>
@@ -59,13 +39,9 @@
       <div class="mt-6 flow-root">
         <div class="-my-6 divide-y divide-gray-500/10">
           <div class="space-y-2 py-6">
-            <NuxtLink
-              v-for="item in navigation"
-              :key="item.name"
-              :href="item.href"
-              class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-              >{{ item.name }}</NuxtLink
-            >
+            <NuxtLink v-for="item in navigation" :key="item.name" :to="item.href"
+              class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+              {{ item.name }}</NuxtLink>
           </div>
           <div class="py-6">
             <UButton to="/auth/login">Sign Up</UButton>
